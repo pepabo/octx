abb253f16a4c30d951722624c752ed529a8a813f
Uchio Kondo <udzura@udzura.jp>
2021-03-19 23:03:29 +0900

    Define record model

--- src/events.rs
+++ src/events.rs
@@ -34,13 +34,26 @@ pub struct IssueEvent {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub commit_id: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub commit_url: Option<String>,
+    pub commit_url: Option<Url>,
     pub created_at: DateTime,
 }
 
 #[derive(Serialize, Debug)]
 struct EventRec {
     pub id: Option<i64>,
+    pub node_id: Option<String>,
+    pub url: Option<String>,
+    pub actor_id: i64,
+    pub assignee_id: Option<i64>,
+    pub assignees: Option<String>,
+    pub assigner_id: Option<i64>,
+    pub labels: Option<String>,
+    pub milestone_name: Option<String>,
+    pub project_card_url: Option<Url>,
+    pub event: Option<String>, // Used instead of Event
+    pub commit_id: Option<String>,
+    pub commit_url: Option<Url>,
+    pub created_at: DateTime,
 
     pub sdc_repository: String,
 }
@@ -49,6 +62,31 @@ impl From<IssueEvent> for EventRec {
     fn from(from: IssueEvent) -> Self {
         Self {
             id: from.id,
+            node_id: from.node_id,
+            url: from.url,
+            actor_id: from.actor.id,
+            assignee_id: from.assignee.map(|u| u.id),
+            assignees: from.assignees.map(|us| {
+                us.iter()
+                    .map(|u| u.login.clone())
+                    .collect::<Vec<String>>()
+                    .join(",")
+            }),
+            assigner_id: from.assigner.map(|u| u.id),
+            labels: from.labels.map(|ls| {
+                ls.iter()
+                    .map(|l| l.name.clone())
+                    .collect::<Vec<String>>()
+                    .join(",")
+            }),
+            milestone_name: from
+                .milestone
+                .map(|m| m.description.unwrap_or("".to_string())),
+            project_card_url: from.project_card.map(|p| p.url),
+            event: from.event,
+            commit_id: from.commit_id,
+            commit_url: from.commit_url,
+            created_at: from.created_at,
 
             sdc_repository: String::default(),
         }
