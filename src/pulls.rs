88fba89234f8916a695148879a8c0e2f130964a1
Uchio Kondo <udzura@udzura.jp>
2021-07-02 19:17:13 +0900

    Required members

--- src/pulls.rs
+++ src/pulls.rs
@@ -27,16 +27,19 @@ pub struct PullRequestFile {
     pub raw_url: Url,
     pub contents_url: Url,
     pub patch: String,
+
+    pub pull_request_number: Option<u64>,
+    pub sdc_repository: Option<String>,
 }
 
-pub struct PullFetcher {
+pub struct PullFileFetcher {
     owner: String,
     name: String,
     since: Option<DateTime<Utc>>,
     octocrab: octocrab::Octocrab,
 }
 
-impl PullFetcher {
+impl PullFileFetcher {
     pub fn new(
         owner: String,
         name: String,
@@ -52,7 +55,7 @@ impl PullFetcher {
     }
 }
 
-impl PullFetcher {
+impl PullFileFetcher {
     pub async fn fetch<T: std::io::Write>(&self, mut wtr: csv::Writer<T>) -> octocrab::Result<()> {
         let param = Params::default();
         let route = format!(
@@ -74,10 +77,13 @@ impl PullFetcher {
                     number = pull.number,
                 );
                 let files_url: Url = self.octocrab.absolute_url(route).unwrap();
-                let files: Vec<PullRequestFile> =
+                let mut files: Vec<PullRequestFile> =
                     self.octocrab.get(&files_url, None::<&()>).await?;
-                for file in files.into_iter() {
-                    wtr.serialize(&file).expect("Serialize failed");
+                for file in files.iter_mut() {
+                    file.pull_request_number = pull.number.into();
+                    file.sdc_repository = format!("{}/{}", self.owner, self.name).into();
+
+                    wtr.serialize(file).expect("Serialize failed");
                 }
 
                 last_update = Some(pull.updated_at.unwrap_or_else(|| pull.created_at));
